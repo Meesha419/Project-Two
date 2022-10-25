42d21caf20c602bf8b14967a60bbccdea6867342
MDeseo <myrnadeseo@gmail.com>
2022-10-26 00:05:55 +1100

    jupyter file upload

---
+++ query.sql
@@ -0,0 +1,12 @@
+-- Query to check successful load
+
+SELECT * FROM  manufacturer;
+
+SELECT * FROM covid_data;
+
+-- Join tables on Country
+
+SELECT manufacturer.location, manufacturer.total vaccinations, covid_data.Country, covid_data.Total_Cases, covid_data.Total_Deaths
+FROM manufacturer
+INNER JOIN covid_data
+ON manufacturer.location = covid_data.Country/Other;
