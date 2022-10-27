e67cc5f13c7f400419c7e09dd4195acc04618303
Meesha419 <rasikasenadheera419@gmail.com>
2022-10-27 20:18:25 +1100

    reverting

--- query.sql
+++ query.sql
@@ -9,4 +9,4 @@ SELECT * FROM covid_data;
 SELECT manufacturer.location, manufacturer.total vaccinations, covid_data.Country, covid_data.Total_Cases, covid_data.Total_Deaths
 FROM manufacturer
 INNER JOIN covid_data
-ON manufacturer.location = covid_data.Country/Other;
+ON manufacturer.location = covid_data.Country;
